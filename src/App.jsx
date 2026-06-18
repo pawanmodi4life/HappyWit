03f9c92aa582b9b57be2a92457e0f8db396b92b8
pawanmodi4life <pawanmodi4life@gmail.com>
2026-06-18 14:34:05 +0530

    Update logo image source in App component

--- src/App.jsx
+++ src/App.jsx
@@ -223,7 +223,7 @@ export default function HappyWitWebsite() {
 
   <div className="flex items-center">
     <img
-      src="https://i.ibb.co/gbW55FPL/Logo.png"
+      src="https://i.ibb.co/p68ZYTDW/Logo.png"
       alt="HappyWit"
       className="h-12 md:h-14 w-auto"
     />
